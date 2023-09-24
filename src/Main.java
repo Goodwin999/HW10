e4653c458c9eff252bf675d5218bc1f1f00c0fd5
Goodwin999 <Vyacheslav.tsitsar@mail.ru>
2023-09-24 22:57:51 +0300

    Fix task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Home Work 10!");
+
+
+        String firstName = " Ivan";
+        String middleName = " Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + firstName + middleName;
+        System.out.println("Ф.И.О. сотрудника -  " + fullName);
+
+
+
     }
+
 }
